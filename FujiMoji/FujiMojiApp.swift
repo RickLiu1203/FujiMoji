1fa83a498c1cade405089a88752bb50300a7f1f1
Rick Liu <rickliu1203@gmail.com>
2025-08-07 00:07:19 -0400

    add initial menubar view

--- FujiMoji/FujiMojiApp.swift
+++ FujiMoji/FujiMojiApp.swift
@@ -10,8 +10,36 @@ import SwiftUI
 @main
 struct FujiMojiApp: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra("🍎") {
+            VStack(alignment: .leading) {
+                ContentView()
+
+                Divider()
+                .padding(.vertical, 4) 
+                
+                Button(action: {
+                    NSApplication.shared.terminate(nil)
+                }){
+                    HStack {
+                        Text("Quit")
+
+                        Spacer()
+
+                        Text("⌘Q")
+                            .foregroundColor(.secondary)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .contentShape(Rectangle())
+                }
+                .keyboardShortcut("q")
+                .buttonStyle(.plain)
+                .frame(maxWidth: .infinity)
+
+            }
+            .padding(16)
+            .frame(width: 200)
+
         }
+        .menuBarExtraStyle(.window)
     }
 }
