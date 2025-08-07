53f788d53049d36f58696e95d652ba1a8cb91b83
Rick Liu <rickliu@Ricks-MacBook-Pro.local>
2025-08-06 23:33:52 -0400

    first commit

---
+++ FujiMoji/FujiMojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FujiMojiApp.swift
+//  FujiMoji
+//
+//  Created by Rick Liu on 2025-08-06.
+//
+
+import SwiftUI
+
+@main
+struct FujiMojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
